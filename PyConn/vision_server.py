2073d32c8f83378ad004a7d2a5949518e8448141
Ramon Qu <quramon@gmail.com>
2017-11-20 19:43:55 -0800

    11/20 laptop server update

--- PyConn/vision_server.py
+++ PyConn/vision_server.py
@@ -7,6 +7,7 @@ from socket import *
 import pickle
 import numpy as np
 import cv2
+import zlib
 from primesense import openni2#, nite2
 from primesense import _openni2 as c_api
 
@@ -19,29 +20,36 @@ def ser():
     s.bind(('', PORT))
     s.listen(5)
     print("listening...")
-    conn, (host, remoteport) = s.accept()
-    size = conn.recv(BUFSIZE)
+    n = 0
+    while True:
+        conn, (host, remoteport) = s.accept()
+        #size = conn.recv(BUFSIZE)
     #receive one number (the size of the array
 
-    arr1 = ""
-    while True:
+        arr1 = b""
+        while True:
             data = conn.recv(BUFSIZE)
             if not data:
                 break
             #parse the data and show the image
-            arr1+=data
+            arr1+=(data)
 
-    canvas = np.hstack((rgbd))
+    #canvas = np.hstack((rgbd))
 
+
+        arr1 = np.fromstring(zlib.decompress(arr1),dtype=np.uint8).reshape(480,640,3)
+        n+=1
     ## Distance map print('Center pixel is {} mm away'.format(dmap[119, 159]))
 
     ## Display the stream
-    cv2.imshow('rgbd', canvas)
+        cv2.imshow('depth', arr1)
+        cv2.waitKey(1)
 
 
-    conn.close()
+        conn.close()
 
 def start_ser():
 
         thread = threading.Thread(target=ser)
         thread.start()
+start_ser()
