18b7a8a4e6c1c1f9b6fcaf4493d2d774750c939a
ramonidea <quramon@gmail.com>
2017-11-20 19:43:02 -0800

    11/18 joule

--- PyConn/test_vision.py
+++ PyConn/test_vision.py
@@ -9,10 +9,11 @@ import numpy as np
 import cv2
 from primesense import openni2#, nite2
 from primesense import _openni2 as c_api
+import zlib
 
 ser_run = True
 BUFSIZE = 10000
-hostAddr = "192.168.137.250"
+hostAddr = "192.168.137.1"
 PORT = 5000
 
 
@@ -33,24 +34,24 @@ else:
 ## Register the device
 dev = openni2.Device.open_any()
 ## create the streams stream
-rgb_stream = dev.create_color_stream()
+#rgb_stream = dev.create_color_stream()
 depth_stream = dev.create_depth_stream()
 ##configure the depth_stream
 #print 'Get b4 video mode', depth_stream.get_video_mode()
-depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX=320, resolutionY=240, fps=30))
-rgb_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888,resolutionX = 320,resolutionY = 240,fps = 30))
+depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX=640, resolutionY=480, fps=30))
+#rgb_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = #c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888,resolutionX = 320,resolutionY = 240,fps = 30))
 ## Check and configure the mirroring -- default is True
 # print 'Mirroring info1', depth_stream.get_mirroring_enabled()
-depth_stream.set_mirroring_enabled(False)
-rgb_stream.set_mirroring_enabled(False)
+depth_stream.set_mirroring_enabled(True)
+#rgb_stream.set_mirroring_enabled(False)
 ## start the stream
-rgb_stream.start()
+#rgb_stream.start()
 depth_stream.start()
 ## synchronize the streams
-dev.set_depth_color_sync_enabled(True) # synchronize the streams
+#dev.set_depth_color_sync_enabled(True) # synchronize the streams
 ## IMPORTANT: ALIGN DEPTH2RGB (depth wrapped to match rgb stream)
-dev.set_image_registration_mode(openni2.IMAGE_REGISTRATION_DEPTH_TO_COLOR)
-
+#dev.set_image_registration_mode(openni2.IMAGE_REGISTRATION_DEPTH_TO_COLOR)
+'''
 def get_rgb():
     """
     Returns numpy 3L ndarray to represent the rgb image.
@@ -58,7 +59,7 @@ def get_rgb():
     bgr   = np.fromstring(rgb_stream.read_frame().get_buffer_as_uint8(),dtype=np.uint8).reshape(240,320,3)
     rgb   = cv2.cvtColor(bgr,cv2.COLOR_BGR2RGB)
     return rgb
-
+'''
 def get_depth():
     """
     Returns numpy ndarrays representing the raw and ranged depth images.
@@ -73,43 +74,30 @@ def get_depth():
         .reshape(240,320) # Used to MATCH RGB Image (OMAP/ARM)
                 Requires .set_video_mode
     """
-    dmap = np.fromstring(depth_stream.read_frame().get_buffer_as_uint16(),dtype=np.uint16).reshape(240,320)  # Works & It's FAST
+    
+    dmap = np.fromstring(depth_stream.read_frame().get_buffer_as_uint16(),dtype=np.uint16).reshape(480,640)  # Works & It's FAST
     d4d = np.uint8(dmap.astype(float) *255/ 2**12-1) # Correct the range. Depth images are 12bits
     d4d = 255 - cv2.cvtColor(d4d,cv2.COLOR_GRAY2RGB)
     return dmap, d4d
 
-def mask_rgbd(d4d,rgb, th=0):
-    """
-    Overlays images and uses some blur to slightly smooth the mask
-    (3L ndarray, 3L ndarray) -> 3L ndarray
-    th:= threshold
-    """
-    mask = d4d.copy()
-    idx =(mask>th)
-    mask[idx] = rgb[idx]
-    return mask
-#mask_rgbd
-
+n = 0
+s = socket(AF_INET, SOCK_STREAM)
 def cli_send(arr1):
-        s = socket(AF_INET, SOCK_STREAM)
-        s.connect((hostAddr, PORT))
+	global s
+        
         #send the size of the array
-        s.send(arr1.size)
+        #s.send(arr1.size)
         #send the combined version of the array (for rgbd)
+	s.sendto(zlib.compress(arr1.tostring()),(hostAddr, PORT))
+        
 
-
-
-
-        s.close()
-
-def send(arr):
-        thread = threading.Thread(target=cli_send,args=[arr])
-        thread.start()
 
 
 s = 0
 done = False
 while not done:
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((hostAddr, PORT))
     key = cv2.waitKey(1) & 255
     ## Read keystrokes
     if key == 27:  # terminate
@@ -117,28 +105,25 @@ while not done:
         done = True
     elif chr(key) == 's':  # screen capture
         print("\ts key detected. Saving image and distance map {}".format(s))
-        #cv2.imwrite("ex5_" + str(s) + '.png', canvas)
         np.savetxt("ex5dmap_" + str(s) + '.out', dmap)
         # s+=1 # uncomment for multiple captures
     # if
 
     ## Streams
-    # RGB
-    rgb = get_rgb()
 
     # DEPTH
     dmap, d4d = get_depth()
-    # Overlay rgb over the depth stream
-    rgbd = mask_rgbd(d4d, rgb)
-    send(rgbd)
-    # canvas
-    #canvas = np.hstack((d4d, rgb, rgbd))
+
+    cli_send(d4d)
+    s.close()
+    cv2.imshow("depth",d4d)
+
 
     ## Distance map
    # print('Center pixel is {} mm away'.format(dmap[119, 159]))
 
-    ## Display the stream
-    #cv2.imshow('depth || rgb || rgbd', canvas)
+
+
 # end while
 
 ## Release resources
