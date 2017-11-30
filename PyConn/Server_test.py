48437ecdb76e58ef5380d48a36a08d038d1219ca
Ramon Qu <quramon@gmail.com>
2017-11-30 13:46:02 -0800

    11/30 update

--- PyConn/Server_test.py
+++ PyConn/Server_test.py
@@ -14,13 +14,10 @@ class Server:
         self.PORT = 5000
         self.s = socket(AF_INET, SOCK_STREAM)
 
-
-    #TODO: 1. Test the size of each frame, whether can be sent in one buffer,
-    #TODO: 2.  if Can, send 4 bit frame size and then receive that amount,
-    #tODO: if cannot,
+    #Normal Data Frame with zlib compression   (Added FPS test)
     def serverBegin(self):
         self.s.bind(('', self.PORT))
-        self.s.listen(5)
+        self.s.listen(200)
         print("listening...")
         lasttime =  int(round(time.time() * 1000))
         count = 0
@@ -29,7 +26,6 @@ class Server:
                 lasttime = int(round(time.time() * 1000))
                 print("Average FPS:"+str(count / 5.0))
                 count = 0
-
             conn, (host, remoteport) = self.s.accept()
             arr1 = b""
             while True:
@@ -38,15 +34,73 @@ class Server:
                     break
                 arr1 += data
             arr1 = self.prepareData(arr1)
-                #np.fromstring(zlib.decompress(arr1), dtype=np.uint8).reshape(480, 640, 3)
-
-            ## Distance map print('Center pixel is {} mm away'.format(dmap[119, 159]))
-            ## Display the stream
+            cv2.waitKey(1) & 255
+            cv2.imshow("Depth", arr1)
+            count+=1
+            conn.close()
+    #Added Time delay in the data Packet
+    def serverBegin2(self):
+        self.s.bind(('', self.PORT))
+        self.s.listen(5)
+        print("listening...")
+        lasttime =  int(round(time.time() * 1000))
+        count = 0
+        timeStamp = 0
+        while True:
+            if(int(round(time.time() * 1000)) - lasttime > 60000):
+                lasttime = int(round(time.time() * 1000))
+                print("Average FPS:"+str(count / 60.0))
+                print("Ave Time Delay: " + str(timeStamp/count*1.0))
+                timeStamp = 0
+                count = 0
+            conn, (host, remoteport) = self.s.accept()
+            arr1 = b""
+            while True:
+                data = conn.recv(self.BUFSIZE)
+                if not data:
+                    break
+                arr1 += data
+            arr1,times  = self.prepareData2(arr1)
+            timeStamp += times
             cv2.waitKey(1) & 255
             cv2.imshow("Depth", arr1)
             count+=1
             conn.close()
 
+
+
+
+
+
+    def prepareData(self,data):
+        data = zlib.decompress(data)
+        data = np.fromstring(data, dtype=np.uint16).reshape(480, 640)
+        d4d = np.uint8(data.astype(float) *255/ 2**12-1)
+        d4d = 255 - cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
+        #d4d = np.fromstring(zlib.decompress(data),dtype=np.uint8).reshape(480,640,3)
+        return d4d
+
+    def prepareData2(self,data): #For parsing the time
+        #data = zlib.decompress(data)
+        tt = round(time.time()*1000)
+        timestamp =abs(int(tt)%10000 - int(data[0:4]))
+        #print(int(tt)%10000,int(data[0:4]),timestamp)
+        data = np.fromstring(data[4:], dtype=np.uint16).reshape(480, 640)
+        d4d = np.uint8(data.astype(float) *255/ 2**12-1)
+        d4d = 255 - cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
+        #d4d = np.fromstring(zlib.decompress(data),dtype=np.uint8).reshape(480,640,3)
+        return d4d,timestamp
+
+    def run(self):
+        thread = threading.Thread(target=self.serverBegin2)
+        thread.start()
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
+
+
+    '''
     def serverBegin1(self):
         self.s.bind(('', self.PORT))
         self.s.listen(1)
@@ -57,6 +111,7 @@ class Server:
             if(int(round(time.time() * 1000)) - lasttime > 5000):
                 lasttime = int(round(time.time() * 1000))
                 print("Average FPS:"+str(count / 5.0))
+
                 count = 0
 
             conn, (host, remoteport) = self.s.accept()
@@ -74,29 +129,11 @@ class Server:
             ## Display the stream
             count+=1
             conn.close()
-
+        
     def show(self,img):
         try:
             cv2.destroyAllWindows()
         except:
             pass
         cv2.imshow("Depth",img)
-        cv2.waitKey(1)&255
-
-
-
-
-    def prepareData(self,data):
-        data = np.fromstring(zlib.decompress(data), dtype=np.uint16).reshape(480, 640)
-        d4d = np.uint8(data.astype(float) *255/ 2**12-1)
-        d4d = 255 - cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
-        #d4d = np.fromstring(zlib.decompress(data),dtype=np.uint8).reshape(480,640,3)
-        return d4d
-
-    def run(self):
-        thread = threading.Thread(target=self.serverBegin)
-        thread.start()
-
-if __name__ == '__main__':
-    server = Server()
-    server.run()
+        cv2.waitKey(1)&255'''
