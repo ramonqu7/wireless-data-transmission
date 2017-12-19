673dd0764c194f552253f3b7bd1bf4800c04fbb6
ramonidea <quramon@gmail.com>
2017-12-19 14:17:57 -0800

    1219 folder change

--- Intel Joule Flash Tools/DNX/get-info-emmc.cmd
+++
@@ -1,5 +0,0 @@
-:: Command file to get eMMC device info
-::
-:: %1 = DNX binary blob
-
-DNX-dldrcli\dldrcli --command getcardinfo --fw_dnx "%~1" --device 2 --idx 0
--- Intel Joule Flash Tools/DNX/reset-system.cmd
+++
@@ -1,5 +0,0 @@
-:: Command file to get eMMC device info
-::
-:: %1 = reset flag
-
-DNX-dldrcli\dldrcli --command startover --flags %~1
