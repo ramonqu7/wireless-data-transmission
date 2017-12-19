fae68ebe463fdcaa35e9099c1d4509ef132a02fb
ramonidea <quramon@gmail.com>
2017-12-19 14:53:28 -0800

    Joule update

--- ROS/publisher.py
+++ ROS/publisher.py
@@ -1,12 +1,29 @@
 from openni2_device_init import VisionSensor
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16MultiArray
 import mraa
 import time
+import zlib
+import rospy
 
 class RgbdPublisher():
     def __init__(self):
         self.device = VisionSensor()
+        self.device.createDepth() # default 640*480*30fps
+        self.RosInit()
+
+    def RosInit(self):
+        self.node = rospy.Publisher('CameraTest')
+        rospy.init_node("Joule", anonymous=False)
+
+    def publishFrame(self):
+        while not rospy.is_shutdown():
+            data = self.device.getDepth()
+            self.node.publish(data)
+
 
 if __name__ == '__main__':
-    rgbd = RgbdPublisher()
+    try:
+        rgbd = RgbdPublisher()
+    except rospy.ROSInterruptException:
+        print("ROS Interrupt")
