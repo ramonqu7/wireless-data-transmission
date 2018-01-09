03dcabe3381fd145937f7beefcc8eb4b853e4855
ramonidea <quramon@gmail.com>
2018-01-08 20:00:42 -0800

    1/8 update

--- ROS/subscriber.py
+++ ROS/subscriber.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int8MultiArray
+import threading
 import time
 import zlib
 
+import bz2
+from std_msgs.msg import Int16MultiArray,String
 import cv2
+import time
 import numpy as np
-import rospy
-from std_msgs.msg import String
 
 count = 0
 lasttime = 0
 
-
 def parseData(data):
-    frame = zlib.decompress(data)
-    frame = np.fromstring(frame, dtype=np.uint8).reshape(480, 640, 3)
+    #frame = zlib.decompress(data)
+    frame = np.fromstring(data, dtype=np.uint16).reshape(480, 640)
     d4d = frame
-    # d4d = np.uint8(frame.astype(float) * 255 / 2 ** 12 - 1)
-    # d4d = 255 - cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
+    d4d = np.uint8(frame.astype(float) * 255 / 2 ** 12 - 1)
+    d4d = 255 - cv2.cvtColor(d4d, cv2.COLOR_GRAY2RGB)
     return d4d
-
 
 def callback(data):
     global count, lasttime
-    # rospy.loginfo(rospy.get_caller_id() + "I heard")
+    #rospy.loginfo(rospy.get_caller_id() + "I heard")
 
     cv2.waitKey(1) & 255
     cv2.imshow("Depth", parseData(data.data))
-    if (int(round(time.time() * 1000)) - lasttime > 30000):
+    if (int(round(time.time() * 1000)) - lasttime > 5000):
         lasttime = int(round(time.time() * 1000))
-        print("Average FPS:" + str(count / 30.0))
+        print("Average FPS:" + str(count / 5.0))
         count = 0
-    count += 1
+    count+=1
 
 
 def listener():
