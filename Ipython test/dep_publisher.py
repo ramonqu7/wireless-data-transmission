54845bb605c5c9808af85ca18bc7e076ca40d247
Ramon Qu <quramon@gmail.com>
2018-02-04 21:32:01 -0800

    Publish depth and rgb different ports

--- Ipython test/dep_publisher.py
+++ Ipython test/dep_publisher.py
@@ -44,4 +44,4 @@ def depth_feed():
     return Response(gen(VideoCamera('depth')),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port = 5000)
+    app.run(host='0.0.0.0', debug=True, port = 5151)
