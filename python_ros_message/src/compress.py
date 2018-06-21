30b4a3cca61772d4d5683ee5e972d8f5f4069f21
Ramon Qu <quramon@gmail.com>
2018-06-20 20:41:07 -0700

    test on compress_decompress

--- python_ros_message/src/compress.py
+++ python_ros_message/src/compress.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import message_filters
 from sensor_msgs.msg import Image
-from rgbd_compress.msg import Rgbd
+from rgbd_compress.msg import rgbd
 import rospy
 import zlib
 import numpy as np
-from PIL import Image
-from io import BytesIO
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -14,12 +12,11 @@ RGB_COMPRESS = 75
 
 def callback(rgb, dep):
     global pub, bridge, RGB_COMPRESS
-    rgbd_msg = Rgbd()
+    rgbd_msg = rgbd()
     rgbd_msg.header = dep.header
-    rgbd_msg.encoding = rgb.encoding
 
     try:
-        rgb_data = bridge.imgmsg_to_cv2(rgb,"brg8")
+        rgb_data = bridge.imgmsg_to_cv2(rgb , "rgb8")
     except CvBridgeError as e:
         print(e)
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), RGB_COMPRESS]
@@ -27,11 +24,10 @@ def callback(rgb, dep):
     rgb_compressed = rgb_compressed.tobytes()
 
     try:
-        dep_data = bridge.imgmsg_to_cv2(dep,"brg8") #may not be brg8
+        dep_data = bridge.imgmsg_to_cv2(dep , "16UC1")  #may not be brg8
     except CvBridgeError as e:
         print(e)
     #Trim down to 1d Array
-    dep_data = np.hsplit(dep_data,3)[0]
     dep_compressed = zlib.compress(dep_data)
 
     final_compressed = rgb_compressed + b'ColDep'+ dep_compressed
@@ -52,11 +48,11 @@ def callback(rgb, dep):
 if __name__ == '__main__':
     bridge = CvBridge()
 
-    rgb_sub = message_filters.Subscriber('rgb/image_raw', Image)
-    dep_sub = message_filters.Subscriber('depth/image_raw', Image)
-    ts = message_filters.TimeSynchronizer([rgb_sub, dep_sub], 10)
+    rgb_sub = message_filters.Subscriber('/camera/color/image_raw', Image)
+    dep_sub = message_filters.Subscriber('/camera/aligned_depth_to_color/image_raw', Image)
+    ts = message_filters.TimeSynchronizer([rgb_sub, dep_sub], 30)
 
-    pub = rospy.Publisher('/camera/rgbd', Rgbd, queue_size=10)
+    pub = rospy.Publisher('/camera/rgbd', rgbd, queue_size=30)
     rospy.init_node('Joule', anonymous=True)
 
     ts.registerCallback(callback)
